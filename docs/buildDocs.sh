aedea301fba46798e4336c72ca8369222c7b07e0
xuruifan <707185547@qq.com>
2024-12-27 19:08:48 +0800

    Update buildDocs.sh

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -28,7 +28,7 @@ python3 -m pip list
   
 pwd
 ls -lah
-git config --global --add safe.directory /__w/test-tutorial/test-tutorial
+git config --global --add safe.directory /__w/tutorial-aspdac2025/tutorial-aspdac2025
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
   
 # make a new temp dir which will be our GitHub Pages docroot
